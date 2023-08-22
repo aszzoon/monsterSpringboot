2cd250f4b728a26d5cc6f174f216015cba58c46d
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2023-08-23 08:09:25 +0900

    first commit

---
+++ src/main/java/com/example/backspringboot/BackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.backspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/backspringboot/service/UsopenService.java
@@ -0,0 +1,9 @@
+package com.example.backspringboot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsopenService {
+
+
+}
